d0d208bd055d0c250068feddf0caf572f5c14dc7
zulc22 <zulc22.db@gmail.com>
2020-12-28 15:50:42 -0800

    0.1.0 -- w00T!1

--- iChicken/iChicken.cpp
+++ iChicken/iChicken.cpp
@@ -1,14 +1,69 @@
 /*
 
 ICHICKEN PLATFORMING ENGINE, WRITTEN BY SCOTT BLACKLOCK
+ADHERES TO SEMANTIC VERSIONING 2.0.0 (https://semver.org/spec/v2.0.0.html)
 
 CHANGELOG
 
-0.0.0 -- Initial Creation [12/27/2020]
-SDL_mixer testing: we now can play MIDIs!
+[ 0.1.0 -- 12/27/2020 ]
+Decoding files from ZIP is working!
 
-assumed deps are:
-vcpkg install sdl2 sdl2-image sdl2-ttf sdl2-mixer[nativemidi] zziplib
+[ 0.0.0 -- 12/27/2020 ]
+Initial creation.
+SDL_mixer testing... we now can play MIDIs!
+
+[ PLAN -- 12/27/2020 ]
+My current plan for this project is to have an asset pipeline that loads any expected
+assets before the level actually starts, and load anything extra on the fly as a fallback.
+I'm thinking of something like a tree of assets -- like so
+
+[ ] GAME FILESYSTEM
+ |
+ +--[ ] INTRO LOGOS / TITLESCREEN
+ |   |
+ |   +-- logo1.png
+ |   +-- logo2.png
+ |   +-- titlescreen.png
+ |   +-- logoscript.???
+ |
+ +--[ ] ROOMS, TILESETS, BACKGROUNDS
+ |   |
+ |   +-- tileset.png
+ |   +-- bg1.png
+ |   +--[ ] room1.???
+ |       |
+ |       +-- Assets to load
+ |       +-- Tileset
+ |       +-- Music track
+ |       +-- Background
+ |       +-- Map data
+ |       +-- Object data (spawnpoint, enemies)
+ |       +-- Collsiion data
+ |
+ +--[ ] SPRITES
+     |
+     +--[ ] player
+     |   |
+     |   +-- spritedefs.???
+     |   +-- walk1.png
+     |   +-- walk2.png
+     |   +-- walk3.png
+     |       ...
+     |
+     +--[ ] enemy
+         |
+         +-- spritedefs.???
+         +-- normal.png
+         +-- attack1.png
+         +-- attack2.png
+         +-- dead.png
+             ...
+
+Current dependencies:
+`vcpkg install sdl2 sdl2-image sdl2-ttf sdl2-mixer[nativemidi] kubazip simpleini`
+
+Since this is basically babby's first C++ game project, it's extremely likely this
+will need many, many, refactors.
 
 */
 
@@ -17,11 +72,17 @@ vcpkg install sdl2 sdl2-image sdl2-ttf sdl2-mixer[nativemidi] zziplib
 
 #include <SDL2/SDL.h>
 #undef main
+
 #include <SDL2/SDL_mixer.h>
 #include <stdio.h>
 #include <string>
 #include <iostream>
 #include <cstdarg>
+#include <SimpleIni.h>
+#include <kubazip/zip/zip.h>
+#include <map>
+
+//UTILITY FUNCTIONS//
 
 std::string concatstrs(int argnum, ...) {
     va_list  valist;
@@ -38,7 +99,13 @@ std::string concatstrs(int argnum, ...) {
     return output;
 }
 
-//// ASSERTION ////
+template<typename K, typename V>
+bool map_has_key(std::map<K,V> map, K key) {
+    auto it = map.find(key);
+    return it != map.end();
+}
+
+///// ASSERTION /////
 
 // Adapted from Konrad Rudolph's SO answer
 // https://stackoverflow.com/a/3767883
@@ -46,7 +113,7 @@ std::string concatstrs(int argnum, ...) {
 #ifndef NDEBUG
 #   define ASSERT(condition, message) \
     do { \
-        if (! (condition)) { \
+        if (condition) { \
             std::cerr << "Assertion `" #condition "` failed in " << __FILE__ \
                       << " line " << __LINE__ << ": " << message << std::endl; \
             std::terminate(); \
@@ -56,7 +123,7 @@ std::string concatstrs(int argnum, ...) {
 #   define ASSERT(condition, message) do { } while (false)
 #endif
 
-//// CWD STUFF ////
+///// CWD STUFF /////
 
 // Adapted from Anvi Jain's article on tutorialspoint
 // https://www.tutorialspoint.com/find-out-the-current-working-directory-in-c-cplusplus
@@ -73,13 +140,104 @@ std::string curdir() {
 #define dirbufsize 1024
 
     char buf[dirbufsize];
-    ASSERT(curdirbuf(buf, dirbufsize) != NULL, "Couldn't get current directory");
+    char* cdbo = curdirbuf(buf, dirbufsize);
+    ASSERT(cdbo == NULL, "Couldn't get current directory");
     return std::string(buf);
 
 #undef dirbufsize
 }
 
-/////////////////// 
+// ASSET MANAGMENT //
+
+#define GAMEZIP "testgame.zip"
+
+std::map<std::string, std::string> ASSETFILENAMES;
+std::map<std::string, std::pair<size_t, char*>> ASSETS;
+
+CSimpleIniA game_ini;
+zip_t* game_zip;
+
+bool ASSETS_has_key(std::string key) {
+    return map_has_key<std::string, std::pair<size_t, char*>>(ASSETS, key);
+}
+bool ASSETFILENAMES_has_key(std::string key) {
+    return map_has_key<std::string, std::string>(ASSETFILENAMES, key);
+}
+
+bool readGameIni = false;
+void checkAssetTable() {
+    if (readGameIni) return;
+    readGameIni = true;
+
+    game_zip = zip_open(GAMEZIP, ZIP_DEFAULT_COMPRESSION_LEVEL, 'r');
+    zip_entry_open(game_zip, "game.ini");
+    ASSETS["game.ini"] = { (size_t)zip_entry_size(game_zip), NULL };
+    ASSETS["game.ini"].second = NULL;
+    zip_entry_read(game_zip, (void**)&ASSETS["game.ini"].second, &ASSETS["game.ini"].first);
+    zip_entry_close(game_zip);
+    ASSETS["game.ini"].second[ASSETS["game.ini"].first] = 0x00;
+
+    SI_Error rc = game_ini.LoadData(ASSETS["game.ini"].second);
+    ASSERT(rc != SI_OK, "Unable to load game.ini");
+
+    std::list<CSimpleIniA::Entry> assets;
+    game_ini.GetAllKeys("assets", assets);
+    for (const CSimpleIniA::Entry ei : assets) {
+        std::cout << ei.pItem << std::endl;
+        ASSETFILENAMES[ei.pItem] = game_ini.GetValue("assets", ei.pItem);
+    }
+    assets.clear();
+}
+
+void loadAsset(std::string name) {
+    checkAssetTable();
+    bool knownname = ASSETFILENAMES_has_key(name);
+    bool loaded = ASSETS_has_key(name);
+    ASSERT(!knownname, "Tried to load unknown asset " << name);
+    if (loaded) return;
+
+    zip_entry_open(game_zip, ASSETFILENAMES[name].c_str());
+    ASSETS[name] = { (size_t)zip_entry_size(game_zip), NULL };
+    zip_entry_read(game_zip, (void**)&ASSETS[name].second, &ASSETS[name].first);
+    zip_entry_close(game_zip);
+}
+/*
+void loadAssets(std::list<std::string> assetNames) {
+    for (const std::string ai : assetNames) {
+        loadAsset(ai);
+    }
+}
+*/
+void unloadAsset(std::string name) {
+    free(ASSETS[name].second);
+    ASSETS.erase(name);
+}
+
+char* getAsset(std::string name) {
+    checkAssetTable();
+    bool knownname = ASSETFILENAMES_has_key(name);
+    bool loaded = ASSETS_has_key(name);
+    if (!loaded && knownname) {
+        std::cout << "WARNING: Loading asset " << name << " on the fly -- this shouldn't happen!" << std::endl;
+        loadAsset(name);
+    }
+    ASSERT(!knownname, "No such asset " << name << " is known.");
+    return ASSETS[name].second;
+}
+
+int getAssetSize(std::string name) {
+    checkAssetTable();
+    bool knownname = ASSETFILENAMES_has_key(name);
+    bool loaded = ASSETS_has_key(name);
+    if (!loaded && knownname) {
+        std::cout << "WARNING: Loading asset " << name << " on the fly -- this shouldn't happen!" << std::endl;
+        loadAsset(name);
+    }
+    ASSERT(!knownname, "No such asset " << name << " is known.");
+    return ASSETS[name].first;
+}
+
+/////////////////////
 
 Mix_Music* music = NULL;
 
@@ -87,17 +245,19 @@ bool initSDLShit() {
     bool t; // temp
     
     t = SDL_Init(SDL_INIT_AUDIO) < 0;
-    ASSERT(!t, "Couldn't initialize SDL! SDL Err." << SDL_GetError());
+    ASSERT(t, "Couldn't initialize SDL! SDL Err." << SDL_GetError());
 
     t = Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0;
-    ASSERT(!t, "Couldn't initialize Mixer! Mix Err." << Mix_GetError());
+    ASSERT(t, "Couldn't initialize Mixer! Mix Err." << Mix_GetError());
     
-    music = Mix_LoadMUS("test.mid");
-    ASSERT(music != NULL, "Couldn't load test.mid! CWD is " << curdir());
+    music = Mix_LoadMUSType_RW(SDL_RWFromMem(getAsset("mus_test"), getAssetSize("mus_test")), MUS_MID, 0);
+    ASSERT(music == NULL, "Couldn't load mus_test!");
+
     return true;
 }
 
 void destruct() {
+    zip_close(game_zip);
     Mix_FreeMusic(music);
     Mix_Quit();
     SDL_Quit();
